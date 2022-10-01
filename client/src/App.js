3626fd4b20d595831a4a8d273af6d771747050e5
reykeem <106338511+reykeem@users.noreply.github.com>
2022-10-01 12:34:12 -0700

    wallet ui wip

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,23 @@ import SignIn from "./components/SignIn";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import CreateForm from "./Components/CreateForm";
+import Marketplace from "./Components/Marketplace";
+import Wallet from "./Components/Wallet";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />}/>
-        {/* <Route path='/marketpalce' element={<MarketPlace />} /> */}
-        <Route path='/create' element={<CreateForm />} />
-        <Route path='/login' element={<SignIn />}/>
-        <Route path='/register' element={<Register />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/marketplace" element={<Marketplace />} />
+        <Route path="/create" element={<CreateForm />} />
+        <Route path="/wallet" element={<Wallet />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </>
-  )
-}
+  );
+};
 
 export default App;
--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -1,18 +1,19 @@
-import axios from 'axios';
-import React, { useEffect, useRef, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import '../styles/forms.css'
+import axios from "axios";
+import React, { useEffect, useRef, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "../styles/forms.css";
 
 const SignIn = () => {
-  const inputRef = useRef(null);  // focus on first input field upon mounting
+  const inputRef = useRef(null); // focus on first input field upon mounting
   const intialState = {
-    username: '',
-    password: ''
+    username: "",
+    password: "",
   };
 
   const [formData, setFormData] = useState(intialState);
   const { username, password } = formData;
   // const [errorMessage, setErrorMessage] = useState(null);  // set errorMessage if fails validation
+  // const navigate = useNavigate();
 
   // focus on first input field upon mounting
   useEffect(() => {
@@ -20,34 +21,53 @@ const SignIn = () => {
   }, []);
 
   const handleChangeForm = (e) => {
-    setFormData(prevState => ({
+    setFormData((prevState) => ({
       ...prevState,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     }));
   };
 
   const handleSignIn = (e) => {
     e.preventDefault();
 
-    // making post request to login user and redirect to homepage
+    // making post request to login user and redirect to homepage if successful
+    // if false, generate error
+    // if true, navigate('/homepoage') we have user's info and can make a GET reuqest to retrieve users' NFTs
+
+    // navigate("/userwallet");
   };
 
   return (
     <main>
-      <form className='form' onSubmit={handleSignIn}>
+      <form className="form" onSubmit={handleSignIn}>
         <h1>Sign in</h1>
-        <div className='form-control'>
+        <div className="form-control">
           <label htmlFor="username">Username</label>
-          <input type="text" id='username' name='username' ref={inputRef} value={username} onChange={handleChangeForm}/>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            ref={inputRef}
+            value={username}
+            onChange={handleChangeForm}
+          />
         </div>
-        <div className='form-control'>
+        <div className="form-control">
           <label htmlFor="password">Password</label>
-          <input type="password" id='password' name='password' value={password} onChange={handleChangeForm}/>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={handleChangeForm}
+          />
         </div>
         {/* {errorMessage && <div className='error-message login'>{errorMessage}</div>} */}
-        <button type='submit'>SIGN IN</button>
-        <div type='button' id='register'><Link to='/register'>Don&apos;t have an account? Sign up</Link></div>
-        <div id='copyright'>NFT Marketplace © 2022</div>
+        <button type="submit">SIGN IN</button>
+        <div type="button" id="register">
+          <Link to="/register">Don&apos;t have an account? Sign up</Link>
+        </div>
+        <div id="copyright">NFT Marketplace © 2022</div>
       </form>
     </main>
   );
