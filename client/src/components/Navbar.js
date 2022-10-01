e3a4fadbe777ad70e0141de5903f0fe14b4c7b19
Brian Pham <brianpham@Brians-Mac-mini.home>
2022-09-30 23:18:34 -0700

    link stylesheet to Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import '../styles/navbar.css'
 
 const Navbar = () => {
   const [currentUser, setCurrentUser] = useState(false);
